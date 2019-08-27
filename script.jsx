1a5f7b36029393e205e76372d48e18968c1afde5
aqilahrajab <YOUR-EMAIL-ADDRESS>
2019-08-27 17:01:48 +0800

    Render function works

--- script.jsx
+++ script.jsx
@@ -1,7 +1,38 @@
-var monkey = "hello";
+
+
+class SingleTweet extends React.Component {
+
+    render () {
+        return (
+            <ul>
+                <li>{this.props.tweetList.user.name}</li>
+                <li>{this.props.tweetList.text}</li>
+            </ul>
+        );
+    }
+}
+
+class AllTweets extends React.Component {
+
+    render() {
+        let tweetsLoop = this.props.tweetList.map( (item, index)  => {
+
+                            return <SingleTweet tweetList = {item}></SingleTweet>;
+                        });
+            return (
+
+                <div>
+                    {tweetsLoop}
+                </div>
+            );
+    }
+}
+
+
+
 ReactDOM.render(
     <div>
-      {monkey}
+        <AllTweets tweetList = {tweets}/>
     </div>,
     document.getElementById('root')
 );
